a989c5aec0ff2073a8cef95b4e58032daba31b3b
Adrian <adrianhyk@gmail.com>
2015-08-14 00:11:25 +0800

    Imported jQuery; Added close button placeholder; Temporarily re-enable taskbar

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 var app = require('app');  // Module to control application life.
+var ipc = require("ipc"); //for renderer-to-app communications
+
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
 
 // Report crashes to our server.
 require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is GCed.
+// Keep a global reference of the window object, if you don't, the window willbe closed automatically when the JavaScript object is GCed.
 var mainWindow = null;
+
+//when a close-request is sent from the renderer (i.e: the close button is clicked), attempt to close the mainWindow
+ipc.on("async-close-request", function(event,arg){
+    console.log(arg);
+    mainWindow.close();
+    //event.sender.send('async-request-reply', 'this is some filler text');
+});
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -21,11 +31,12 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
     // Create the browser window.
-    var browserWindowSettings = {width: 300, height: 400, "always-on-top": true, "frame": false};
+    //var browserWindowSettings = {width: 300, height: 400, "always-on-top": true, "frame": false};
+    var browserWindowSettings = {width: 300, height: 400, "always-on-top": true};
     mainWindow = new BrowserWindow(browserWindowSettings);
 
     // and load the index.html of the app.
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.loadUrl('file://' + __dirname + '/html/index.html');
 
     // Open the devtools.
     //mainWindow.openDevTools();
